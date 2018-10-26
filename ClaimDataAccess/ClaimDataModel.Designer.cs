1d17806c1f0072db5785feda08c8335e963035a4
ursravis <urs.ravis@live.com>
2018-10-25 18:04:47 -0700

    ECS support

--- ClaimDataAccess/ClaimDataModel.Designer.cs
+++ ClaimDataAccess/ClaimDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Personal\ProcessClaimService\ClaimDataAccess\ClaimDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\GitRep\ClaimWebAPI\ClaimDataAccess\ClaimDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
